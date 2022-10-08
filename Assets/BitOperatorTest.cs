e698e779090e45a66e37ba4069d259438f68c79f
RodMaker <83619630+RodMaker@users.noreply.github.com>
2022-10-08 13:30:43 +0100

    initial commit

---
+++ Assets/BitOperatorTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BitOperatorTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        uint a, b, c;
+        a = 32;
+        b = a >> 3;
+        c = a << 3;
+        Debug.Log($"a,b,c={a} {b} {c}");
+
+        uint d, e, f;
+        
+        a = 0x04700_8999;
+        b = 0x0ffff_0000;
+
+        c = a & b;  // and
+        d = a | b;  // or
+        e = a ^ b;  // xor
+        f = ~a;     // complement
+
+        Debug.Log($"a={a:x}");
+        Debug.Log($"b={b:x}");
+        Debug.Log($"c={c:x}");
+        Debug.Log($"d={d:x}");
+        Debug.Log($"e={e:x}");
+        Debug.Log($"f={f:x}");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/OperatorTest.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OperatorTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int i, j, k, answer;
+        i = 2;
+        j = 3;
+        k = 4;
+
+        /*
+        answer = i + j + k;
+        Debug.Log($"answer={answer}");
+        answer = i + j * k;
+        Debug.Log($"answer={answer}");
+        answer = (i + j) * k;
+        Debug.Log($"answer={answer}");
+        answer = i + j + k * 1000;
+        Debug.Log($"answer={answer}");
+        answer = (i + j + k) * 1000;
+        Debug.Log($"answer={answer}");
+        answer = k / 3;
+        Debug.Log($"answer={answer}");
+        answer = k % 3;
+        Debug.Log($"answer={answer}");
+        answer = (i + j + k) % 7;
+        Debug.Log($"answer={answer}");
+        */
+
+        /*
+        answer = 10;
+        answer++;
+        Debug.Log($"answer={answer}");
+        answer--;
+        Debug.Log($"answer={answer}");
+        ++answer;
+        Debug.Log($"answer={answer}");
+        --answer;
+        Debug.Log($"answer={answer}");
+        */
+
+        /*
+        answer = 10;
+        Debug.Log($"answer={answer++}");
+        Debug.Log($"answer={answer}");
+        answer = 20;
+        Debug.Log($"answer={++answer}");
+        Debug.Log($"answer={answer}");
+        */
+
+        /*
+        answer = 10;
+        Debug.Log($"answer={+answer}");
+        Debug.Log($"answer={-answer}");
+        */
+
+        /*
+        Debug.Log("Floating Test");
+        float x, y;
+        double z;
+        x = 10.1f;
+        z = 50.12345123451234512345;
+        y = x + 10;
+        y = x + (float)z;
+        Debug.Log($"x y z={x} {y} {z}");
+        */
+
+        //int i = 2;
+        i += 7;
+        Debug.Log($"i={i}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
